7dbf5c8d0ec8f9a0dc2eabbca26a66946d921463
Waldsson Sacramento <waldsson.dev@gmail.com>
2023-05-15 23:11:13 -0300

    Dia 03

--- primeiros-passos/atualizar-personagem.sql
+++ primeiros-passos/atualizar-personagem.sql
@@ -1,3 +1,19 @@
 SELECT * FROM personagem;
 --Comando para atualizar informações de determinado personagem
 UPDATE personagem SET nome = 'Luke Skywalker' WHERE id = 1;
+
+--Alterar id do personagem, limitando a somente uma alteração
+UPDATE personagem set id = 100 WHERE id = 2 LIMIT 1;
+UPDATE personagem set id = 5 WHERE id = 4 LIMIT 1;
+
+UPDATE personagem set nome = 'Leia Organo' WHERE id = 4
+
+-- COMMIT diário ADD
+
+UPDATE personagem SET nome = 'Luke Skywalker' WHERE id = 1;
+
+--Alterar id do personagem, limitando a somente uma alteração
+UPDATE personagem set id = 100 WHERE id = 2 LIMIT 1;
+UPDATE personagem set id = 5 WHERE id = 4 LIMIT 1;
+
+UPDATE personagem set nome = 'Leia Organo' WHERE id = 4
